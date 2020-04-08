f0d4070c93c3d8a9d8a2feff075e2267e03b1bfa
Sowa Killian <killian.sowa@m6.fr>
2020-04-08 15:09:55 +0200

    files init and firebase configured

---
+++ Pods/Target Support Files/Pods-PoloTravel/Pods-PoloTravel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PoloTravel : NSObject
+@end
+@implementation PodsDummy_Pods_PoloTravel
+@end
---
+++ PoloTravel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PoloTravel
+//
+//  Created by SOWA KILLIAN on 08/04/2020.
+//  Copyright © 2020 PoloTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
