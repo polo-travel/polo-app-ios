007960f2b0a7d6b06ae0e8f0c3afcc81e6485865
Sowa Killian <killian.sowa@m6.fr>
2020-04-17 14:35:06 +0200

    fonts, colors and UIFiles architecture fixed

---
+++ PoloTravel/UIFiles/Constants/ColorConstants.swift
@@ -0,0 +1,16 @@
+//
+//  ColorConstants.swift
+//  PoloTravel
+//
+//  Created by SOWA KILLIAN on 17/04/2020.
+//  Copyright © 2020 PoloTeam. All rights reserved.
+//
+
+import UIKit
+
+enum Colors {
+    static let mainDarkBlue = UIColor(red: 15/255, green: 35/255, blue: 53/255, alpha: 1.0)
+    static let mainLightBlue = UIColor(red: 163/255, green: 217/255, blue: 237/255, alpha: 1.0)
+    static let mainRed = UIColor(red: 211/255, green: 28/255, blue: 33/255, alpha: 1.0)
+    static let mainYellow = UIColor(red: 255/255, green: 238/255, blue: 80/255, alpha: 1.0)
+}
