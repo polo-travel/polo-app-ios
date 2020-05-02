1aa90cc965ac5c005dcb1c38f4f83aa3291555d8
naelmessaoudene <naelmessaoudene@gmail.com>
2020-05-02 17:31:37 +0200

    add multiple buttons w/ choice

--- PoloTravel/ViewControllers/TravelCreationViewControllers/Step2TravelCreationViewController.swift
+++ PoloTravel/ViewControllers/TravelCreationViewControllers/Step2TravelCreationViewController.swift
@@ -11,18 +11,40 @@ import UIKit
 class Step2TravelCreationViewController: UIViewController {
 
     @IBOutlet weak var nextButton: BasicButton!
+    @IBOutlet weak var buttonWrapper: UIView!
     
+    @IBOutlet var buttons: [UIButton]!
+
     let questions = ["J’adore l’adrénaline !!","Bof, mais je le fais quand même", "Jamais de la vie"]
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
-        // Do any additional setup after loading the view.
-        customButton()
+        customNextButton()
+        getBtn()
+    
+    }
+
+
+    func getBtn(){
+        for view in buttonWrapper.subviews as [UIView] {
+            if let btn = view as? BasicButton {
+                btn.setWhiteButton()
+                btn.setTitle(questions[btn.tag], for: .normal)
+            }
+        }
     }
     
+    
+    @IBAction func show(sender: UIButton) {
 
-    func customButton(){
+         self.buttons.forEach { (button) in
+            button.backgroundColor = (button === sender) ? UIColor.MainTheme.mainDarkBlue : .white
+            button.setTitleColor((button === sender) ? .white : UIColor.MainTheme.mainDarkBlue, for: .normal)
+        }
+        print(sender.tag)
+    }
+  
+    func customNextButton(){
         
         nextButton.setImage(UIImage(named: "right_arrow"), for: .normal)
         nextButton.imageEdgeInsets = UIEdgeInsets(top: 0,left: 0,bottom: 0,right: 0)
@@ -31,14 +53,5 @@ class Step2TravelCreationViewController: UIViewController {
         nextButton.setDarkButton()
 
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
