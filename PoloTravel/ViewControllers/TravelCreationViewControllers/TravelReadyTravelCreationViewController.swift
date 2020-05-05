ffcb91da6e63947f5f06b7bc061b69c106b56238
Sowa Killian <killian.sowa@m6.fr>
2020-05-05 14:46:51 +0200

    viewcontroller added for last step

---
+++ PoloTravel/ViewControllers/TravelCreationViewControllers/TravelReadyTravelCreationViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TravelReadyTravelCreationViewController.swift
+//  PoloTravel
+//
+//  Created by SOWA KILLIAN on 05/05/2020.
+//  Copyright © 2020 PoloTeam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TravelReadyTravelCreation1ViewController: UIViewController {
+
+    @IBOutlet weak var buttonSeeTravel: BasicButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        buttonSeeTravel.setDarkButton()
+    }
+    
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+    }
+
+}
