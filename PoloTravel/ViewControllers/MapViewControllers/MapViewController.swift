fa49406305c1f2ed5b22868d74a6aad3df5b159c
naelmessaoudene <naelmessaoudene@gmail.com>
2020-05-05 19:52:25 +0200

    wip on itinerary creation

--- PoloTravel/ViewControllers/MapViewControllers/MapViewController.swift
+++ PoloTravel/ViewControllers/MapViewControllers/MapViewController.swift
@@ -12,19 +12,45 @@ import MapboxDirections
 import MapboxCoreNavigation
 import MapboxNavigation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MGLMapViewDelegate {
 
+    var mapView: NavigationMapView!
+    var navigateButton: BasicButton!
+    
+    let lyonCoord = CLLocationCoordinate2D(latitude: 45.75, longitude: 4.85)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let mapView = MGLMapView(frame: view.bounds, styleURL: MGLStyle.darkStyleURL)
+    
+        mapView = NavigationMapView(frame: view.bounds, styleURL: MGLStyle.lightStyleURL)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        mapView.showsUserLocation = true
         
-        mapView.setUserTrackingMode(.follow, animated: true)
         view.addSubview(mapView)
-
+        
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        mapView.setUserTrackingMode(.follow, animated: true)
+        customNavigateButton()
+       // mapView.zoomLevel = 6.0
         // Do any additional setup after loading the view.
     }
     
+    func customNavigateButton(){
+        navigateButton = BasicButton(frame: CGRect(x:(view.frame.width/2 ) - 100, y: view.frame.height - 150, width: 200, height:50 ))
+        navigateButton.setDarkButton()
+        navigateButton.setTitle("NAVIGATE", for: .normal)
+        navigateButton.layer.zPosition  = 9
+        navigateButton.addTarget(self, action: #selector(navigateButtonPressed(_:)), for: .touchUpInside)
+        view.addSubview(navigateButton)
+    }
+    
+    @objc func navigateButtonPressed(_ sender: BasicButton){
+        
+        print("button pressed")
+        
+    }
+    
+    
+    
+
 }
