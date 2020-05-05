11b7139ff2d53543fb4b2ca0e7daa37dc8f4ec9c
naelmessaoudene <naelmessaoudene@gmail.com>
2020-05-05 16:15:26 +0200

    display current location

--- PoloTravel/ViewControllers/MapViewControllers/MapViewController.swift
+++ PoloTravel/ViewControllers/MapViewControllers/MapViewController.swift
@@ -16,26 +16,15 @@ class MapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let url = URL(string: "mapbox://styles/mapbox/streets-v11")
-        let mapView = MGLMapView(frame: view.bounds, styleURL: url)
+        
+        let mapView = MGLMapView(frame: view.bounds, styleURL: MGLStyle.darkStyleURL)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        mapView.setCenter(CLLocationCoordinate2D(latitude: 59.31, longitude: 18.06), zoomLevel: 9, animated: false)
+        mapView.showsUserLocation = true
+        
+        mapView.setUserTrackingMode(.follow, animated: true)
         view.addSubview(mapView)
-
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
