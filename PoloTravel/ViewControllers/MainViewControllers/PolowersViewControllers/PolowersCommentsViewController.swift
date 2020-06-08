2ce43aebc3b723f3bb7d073233bf286703620d49
Sowa Killian <45595422+sowakillian@users.noreply.github.com>
2020-06-08 23:05:49 +0200

    fix(polowers): images are now added in the good order (#52)
    Co-authored-by: Sowa Killian <killian.sowa@m6.fr>

--- PoloTravel/ViewControllers/MainViewControllers/PolowersViewControllers/PolowersCommentsViewController.swift
+++ PoloTravel/ViewControllers/MainViewControllers/PolowersViewControllers/PolowersCommentsViewController.swift
@@ -12,21 +12,23 @@ class PolowersCommentsViewController: UIViewController,UINavigationControllerDel
 
     @IBOutlet weak var img: UIImageView!
     @IBOutlet weak var lbl: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
     
-    @IBOutlet weak var allComments: UIButton!
-    @IBOutlet weak var commentView: UIView!
+
     @IBOutlet weak var commentImage: UIImageView!
     @IBOutlet weak var userImage: UIImageView!
     var name = ""
     var imgURL = ""
+    var desc = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print(imgURL)
         
         lbl.text = name
-        allComments.layer.zPosition = 55
-
+        descriptionLabel.text = desc
+        
         img.sd_setImage(with: URL(string: imgURL), placeholderImage: UIImage(named: "photo.png"))
         img.layer.cornerRadius = 20
                img.clipsToBounds = true
@@ -40,17 +42,11 @@ class PolowersCommentsViewController: UIViewController,UINavigationControllerDel
               commentImage.layer.cornerRadius = 20
               commentImage.clipsToBounds = true
 
-
-        
-        commentView.layer.borderWidth = 10
-        commentView.layer.borderColor = UIColor.green.cgColor
-
     }
     
 
     @IBAction func goBackButton(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
-        //self.navigationController?.popViewController(animated: true)
     }
     
 }
